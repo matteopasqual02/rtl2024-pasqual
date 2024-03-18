1ea4b03fbc159cd35b253bd2ff6c07a85028bf1e
matteopasqual02 <160610524+matteopasqual02@users.noreply.github.com>
2024-03-18 14:08:12 +0100

    Add files via upload

--- project_reti_logiche.vhd
+++ project_reti_logiche.vhd
@@ -38,7 +38,7 @@ architecture Behavioral of project_reti_logiche is
 
 --begin process          
 begin
-    lambdafunction : process (i_clk,i_rst)    --only when current_state or i_start is updated
+    programfunction : process (i_clk,i_rst)    --only when current_state or i_start is updated
         variable current_credibility : std_logic_vector (7 downto 0);  --current credibility
         variable next_credibility : std_logic_vector (7 downto 0);     --next credibility = current credibility -1
         
@@ -46,10 +46,11 @@ begin
       
         variable current_temp_add : std_logic_vector(15 downto 0);     --current RAM address (from i_add to i_add + i_k)
         variable next_temp_add : std_logic_vector(15 downto 0);        --next RAM address = current RAM address +2
+        
     begin
             
         if i_rst = '1' then
-        -- reset state: all signals set at 0 and when i_start is setted to 1: (next_state <= S_R2R;)
+        -- reset signal: all signals set at 0 and when i_start is setted to 1: (next_state <= S_R2R;)
             o_done <= '0';
             o_mem_en <= '0';
             o_mem_we <= '0';
@@ -66,8 +67,8 @@ begin
             case current_state is
             
                 when S_RST =>
+                --RESET STATE: wait i_start then RAM memory Ready To Read
                     if i_start = '1' then
-                    --RAM memory Ready To Read
                         o_mem_addr <= i_add;
                         current_temp_add := i_add;
                         o_mem_en <= '1';
@@ -85,11 +86,14 @@ begin
                when S_CHOICE =>  
                --RAM data available: make the choice
                     if i_mem_data = "00000000" and current_temp_add = i_add then
+                    --sequence starts with 0
                         current_state <= S_ZERO;
                     elsif i_mem_data = "00000000" then
+                    --current word is 0
                         current_state <= S_R2W_NUMPREC;
                         current_credibility := next_credibility;
                     else
+                    -- current word is a number
                         current_state <= S_R2W_31;
                         current_word := i_mem_data;
                         next_credibility := "00011110";
@@ -97,12 +101,14 @@ begin
                     end if;
                     
                 when S_R2W_31 =>
+                --set memory read to write at ADD +1
                     o_mem_addr <= std_logic_vector(SIGNED(current_temp_add) + 1);
                     o_mem_en <= '1';
                     o_mem_we <= '1';
                     current_state <= S_WRITE_MEM_CRED31;
                                    
                 when S_WRITE_MEM_CRED31 =>
+                --write credibility = 31 and update next temp add
                     o_mem_data <= "00011111";
                     o_mem_en <= '1';
                     o_mem_we <= '1';
@@ -110,11 +116,13 @@ begin
                     current_state <= S_MOVE_ADD2; 
                     
                 when S_R2W_NUMPREC =>
+                --set memory read to write at ADD 
                      o_mem_en <= '1';
                      o_mem_we <= '1';
                      current_state <= S_WRITE_MEM_NUMPREC;
                                     
                 when S_WRITE_MEM_NUMPREC =>
+                --write the previous word
                      o_mem_data <= current_word;
                      o_mem_en <= '1';
                      o_mem_we <= '1';
@@ -122,12 +130,14 @@ begin
                      current_state <= S_R2W_CREDX;
                                                     
                 when S_R2W_CREDX =>
+                --set memory read to write at ADD +1
                      o_mem_addr <= std_logic_vector(SIGNED(current_temp_add) + 1);
                      o_mem_en <= '1';
                      o_mem_we <= '1';
                      current_state <= S_WRITE_MEM_CREDX;
                                     
                 when S_WRITE_MEM_CREDX =>
+                --write credibility = 31 and update next temp add
                      o_mem_data <= current_credibility;
                      if current_credibility >= "00000001" then
                         next_credibility := std_logic_vector(SIGNED(current_credibility) - 1);
@@ -137,12 +147,15 @@ begin
                      current_state <= S_MOVE_ADD2;
                                     
                 when S_MOVE_ADD2 =>
+                --check if the sequence is finished ADD' >= ADD + K
                      if SIGNED(next_temp_add) >= 2*SIGNED(i_k) + SIGNED(i_add)  then
+                     --set o_done to 1 and go to the end
                         current_state <= S_END;
                         o_mem_en <= '0';
                         o_mem_we <= '0';
                         o_done <= '1';                 
-                      else
+                     else
+                     --restart with a new read 
                         current_temp_add := next_temp_add;
                         o_mem_addr <=next_temp_add;
                         o_mem_en <= '1';
@@ -152,16 +165,20 @@ begin
                         end if;
                                         
                 when S_END =>
+                --wait that i_start coe back to 0 then reset o_done to 0
                         if i_start <= '0' then 
                               o_done <= '0';
                         end if;
                         current_state <= S_END;
                         
                 when S_ZERO =>
+                --Check if the sequence is finisshed 
                     if SIGNED(current_temp_add) >= 2*SIGNED(i_k) + SIGNED(i_add) -2  then
+                    --go to end state 
                         current_state <= S_END;
                         o_done <= '1'; 
                     else
+                    --ready to read a new value
                         next_temp_add := std_logic_vector(SIGNED(current_temp_add) + 2);
                         o_mem_addr <=next_temp_add;
                         o_mem_en <= '1';
@@ -170,12 +187,14 @@ begin
                     end if;
                     
                 when S_ZERO_READ =>
+                --read + freeze
                     current_temp_add := next_temp_add;
                     o_mem_en <= '0';
                     o_mem_we <= '0';
                     current_state <= S_ZERO_CHOICE;
                 
                 when S_ZERO_CHOICE =>
+                --make the choice again
                     if i_mem_data = "00000000" then
                         current_state <= S_ZERO;
                     else
